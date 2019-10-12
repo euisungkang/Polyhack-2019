dba8dfb4f0b1ede001e22299f3ae0dfcfc5722d0
Jonathan Liu <jonyliu99@gmail.com>
2019-10-11 22:40:43 -0400

    Add Easy Weapons, Movement Script

---
+++ Polyhack Gamu/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRigidBody;
+    public GameObject projectile;
+    public float moveSpeed;
+    Camera viewCamera;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigidBody = GetComponent<Rigidbody>();
+        viewCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //movements
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        //playerRigidBody.velocity = movement * moveSpeed;
+        playerRigidBody.MovePosition(playerRigidBody.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+        //aim direction
+        Ray ray = viewCamera.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayDistance;
+
+        if (groundPlane.Raycast(ray, out rayDistance))
+        {
+            Vector3 point = ray.GetPoint(rayDistance);
+            Vector3 straightPoint = new Vector3(point.x, transform.position.y, point.z);
+            transform.LookAt(straightPoint);
+        }
+
+        if(Input.GetKeyDown("space"))
+        {
+            GameObject bullet = Instantiate(projectile, transform.position, Quaternion.identity) as GameObject;
+            bullet.GetComponent<Rigidbody>().AddForce(transform.forward * 10);
+        }
+    }
+}
